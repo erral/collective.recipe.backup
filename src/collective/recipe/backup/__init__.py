83ea771083668ad127da5d58e94d7436da8b5c1d
Maurits van Rees <maurits@vanrees.org>
2017-02-24 01:39:55 +0100

    Undid a whitespace change.

--- src/collective/recipe/backup/__init__.py
+++ src/collective/recipe/backup/__init__.py
@@ -372,7 +372,6 @@ class Recipe(object):
             loglevel = 'DEBUG'
         else:
             loglevel = 'INFO'
-
         initialization_template = """
 import logging
 loglevel = logging.%(loglevel)s
